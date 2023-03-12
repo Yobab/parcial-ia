6d220fe132b1667fda4b55cd2e6fba6b21940fa0
Yobab <yjavier@miumg.edu.gt>
2023-03-11 19:26:59 -0600

    Inicial commit

---
+++ a.py
@@ -0,0 +1,9 @@
+# Crear una lista de listas de 5 registros de 3 registros cada uno
+nueva_lista = [["rojo", "verde", "azul"], ["amarillo", "naranja", "rosa"], ["blanco", "negro", "gris"], 
+               ["morado", "celeste", "turquesa"], ["dorado", "plateado", "bronce"]]
+
+# Imprimir el cuarto valor del tercer registro 
+print(nueva_lista[2][3])
+
+# Imprimir el tercer valor del segundo registro 
+print(nueva_lista[1][2])
